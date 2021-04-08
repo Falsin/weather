09d2e36536c3c0568add7e54b16bc618a51a2ee0
Falsin <greenldend@gmail.com>
2021-04-08 22:13:23 +0700

    refactoring the showCityIntoConsole function

--- src/index.js
+++ src/index.js
@@ -7,9 +7,28 @@ btn.addEventListener('mousedown', () => {
   showCityIntoConsole()
 })
 
-function showCityIntoConsole() {
+function showCityIntoConsole(params) {
   const cityName = input.value;
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7583fd4c80f1f8e75fe03f14d121ece0`, {mode: 'cors'})
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=7583fd4c80f1f8e75fe03f14d121ece0', {mode: 'cors'})
+  .then(response => response.json())
+  .then(response => {
+    let coord = {
+      lat: response.coord.lat,
+      lon: response.coord.lon,
+    };
+    return coord;
+  })
+  .then(response => {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${response.lat}&lon=${response.lon}&appid=7583fd4c80f1f8e75fe03f14d121ece0`, {mode: 'cors'})
+    .then(response => {
+      console.log(response.json());
+    })
+  })
+}
+
+/* function showCityIntoConsole() {
+  const cityName = input.value;
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&exclude=daily&appid=7583fd4c80f1f8e75fe03f14d121ece0`, {mode: 'cors'})
   .then(response => {
     let answer = response.json();
     console.log(answer);
@@ -26,11 +45,22 @@ function showCityIntoConsole() {
     console.log(response.feels_like + " Fahrenheit")
     console.log('Привет!')
   })
-}
+} */
 
 window.onload = () => {
   fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=7583fd4c80f1f8e75fe03f14d121ece0', {mode: 'cors'})
+  .then(response => response.json())
   .then(response => {
-    console.log(response.json());
+    let coord = {
+      lat: response.coord.lat,
+      lon: response.coord.lon,
+    };
+    return coord;
+  })
+  .then(response => {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${response.lat}&lon=${response.lon}&appid=7583fd4c80f1f8e75fe03f14d121ece0`, {mode: 'cors'})
+    .then(response => {
+      console.log(response.json());
+    })
   })
 }
