8d9ba568967c099772f22b437284ccac29baaeed
Falsin <greenldend@gmail.com>
2021-04-09 21:10:52 +0700

    setting the todayWeather block, added the create-html-node module and added a setTodayWeather function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import './style.less';
-  
+import createElem from 'create-html-node'
+
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
-const cityName = document.querySelector('h2');
 
-btn.addEventListener('mousedown', () => {
-  processingRequest(input.value);
-})
+const todayWeather = document.getElementById('todayWeather');
 
-window.onload = () => {
-  processingRequest('London')
-}
+
+btn.addEventListener('mousedown', () => processingRequest(input.value))
+
+window.onload = () => processingRequest('London');
 
 function processingRequest(currentCityName) {
   returnCityCoord(currentCityName)
@@ -18,7 +17,7 @@ function processingRequest(currentCityName) {
       return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${response.lat}&lon=${response.lon}&appid=7583fd4c80f1f8e75fe03f14d121ece0`, {mode: 'cors'})
     })
     .then(response => response.json())
-    .then(response => addInfoIntoTheSite(response))
+    .then(response => addInfoIntoTheSite(response, currentCityName))
 }
 
 function returnCityCoord(currentCityName) {
@@ -27,9 +26,31 @@ function returnCityCoord(currentCityName) {
     .then(response => response.coord)
 }
 
-function addInfoIntoTheSite(obj) {
-  console.log(obj);
+function addInfoIntoTheSite(obj, currentCityName) {
+  //console.log(obj);
+  setTodayWeather(obj.current, obj.hourly, currentCityName)
 }
+
+function setTodayWeather(currentWeather, hourlyWeather, currentCityName) {
+  const cityName = todayWeather.querySelector('h2');
+  const temp = todayWeather.querySelector('.temp');
+  const hourlyWeatherBlock = document.getElementById('hourlyWeather');
+
+  cityName.textContent = currentCityName;
+  mainWeather.textContent = currentWeather.weather[0].main;
+  temp.textContent = currentWeather.temp;
+
+
+  for (let i = 0; i < 24; i++) {
+    const elem = createElem(hourlyWeatherBlock, 'div', 'Hello!')
+  }
+  //console.log(hourlyWeather)
+/*   hourlyWeather.forEach(element => {
+    console.log(new Date(element.dt * 1000).getHours())
+  }); */
+}
+
+
 
 /* window.onload = () => {
   const currentCityName = 'London';
