cca9a2ddfb8c55c6dea97d85223ec9c9e2b4d09a
Falsin <greenldend@gmail.com>
2021-04-09 11:56:31 +0700

    setting the header section

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ btn.addEventListener('mousedown', () => {
 
 function showCityIntoConsole(params) {
   const cityName = input.value;
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=7583fd4c80f1f8e75fe03f14d121ece0', {mode: 'cors'})
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7583fd4c80f1f8e75fe03f14d121ece0`, {mode: 'cors'})
   .then(response => response.json())
   .then(response => {
     let coord = {
@@ -26,27 +26,6 @@ function showCityIntoConsole(params) {
   })
 }
 
-/* function showCityIntoConsole() {
-  const cityName = input.value;
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&exclude=daily&appid=7583fd4c80f1f8e75fe03f14d121ece0`, {mode: 'cors'})
-  .then(response => {
-    let answer = response.json();
-    console.log(answer);
-    return answer;
-  })
-  .then(response => {
-    let answer = response.main;
-    console.log(response.name)
-    console.log(answer)
-    return answer;
-  })
-  .then(response => {
-    console.log(response.temp + " Fahrenheit");
-    console.log(response.feels_like + " Fahrenheit")
-    console.log('Привет!')
-  })
-} */
-
 window.onload = () => {
   fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=7583fd4c80f1f8e75fe03f14d121ece0', {mode: 'cors'})
   .then(response => response.json())
@@ -60,7 +39,13 @@ window.onload = () => {
   .then(response => {
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${response.lat}&lon=${response.lon}&appid=7583fd4c80f1f8e75fe03f14d121ece0`, {mode: 'cors'})
     .then(response => {
-      console.log(response.json());
+      let answer = response.json();
+      console.log(answer);
+      return answer;
+    })
+    .then(response => {
+      let currentDate = new Date(response.current.dt * 1000);
+      console.log(currentDate)
     })
   })
 }
