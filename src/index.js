b721aa3294fde048dc26bb7d44721ca42bc19d94
Falsin <greenldend@gmail.com>
2021-04-09 12:47:31 +0700

    refactoring code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,59 @@ import './style.less';
   
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
+const cityName = document.querySelector('h2');
 
 btn.addEventListener('mousedown', () => {
+  processingRequest(input.value);
+})
+
+window.onload = () => {
+  processingRequest('London')
+}
+
+function processingRequest(currentCityName) {
+  returnCityCoord(currentCityName)
+    .then(response => {
+      return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${response.lat}&lon=${response.lon}&appid=7583fd4c80f1f8e75fe03f14d121ece0`, {mode: 'cors'})
+    })
+    .then(response => response.json())
+    .then(response => addInfoIntoTheSite(response))
+}
+
+function returnCityCoord(currentCityName) {
+  return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${currentCityName}&appid=7583fd4c80f1f8e75fe03f14d121ece0`, {mode: 'cors'})
+    .then(response => response.json())
+    .then(response => response.coord)
+}
+
+function addInfoIntoTheSite(obj) {
+  console.log(obj);
+}
+
+/* window.onload = () => {
+  const currentCityName = 'London';
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${currentCityName}&appid=7583fd4c80f1f8e75fe03f14d121ece0`, {mode: 'cors'})
+  .then(response => response.json())
+  .then(response => {
+    let coord = {
+      lat: response.coord.lat,
+      lon: response.coord.lon,
+    };
+    return coord;
+  })
+  .then(response => {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${response.lat}&lon=${response.lon}&appid=7583fd4c80f1f8e75fe03f14d121ece0`, {mode: 'cors'})
+    .then(response => {
+      return response.json()
+    })
+    .then(response => {
+      cityName.textContent = currentCityName;
+      console.log(response)
+    })
+  })
+} */
+
+/* btn.addEventListener('mousedown', () => {
   showCityIntoConsole()
 })
 
@@ -48,4 +99,4 @@ window.onload = () => {
       console.log(currentDate)
     })
   })
-}
+} */
